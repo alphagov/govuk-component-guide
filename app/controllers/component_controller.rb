10d7604d5475b1768455c57515046a59b822ebbd
David Singleton <david.singleton@digital.cabinet-office.gov.uk>
2014-08-06 16:25:19 +0000

    Dynamic component view, example with locals

--- app/controllers/component_controller.rb
+++ app/controllers/component_controller.rb
@@ -1,9 +1,10 @@
 class ComponentController < ApplicationController
   def show
     @component_name = params[:name]
-
     render :status => 404 unless available_components.has_key?(@component_name)
 
+    @component = available_components[@component_name]
+    @component_params = @component[:params]
   end
 
   def list
@@ -13,7 +14,12 @@ class ComponentController < ApplicationController
 private
   def available_components
     {
-        "beta_label" => {}
+        "beta_label" => {},
+        "example" => {
+          :params => {
+            :content => "Hello!"
+          }
+        }
     }
   end
 end
