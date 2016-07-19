db2dd0f1f4d95f8f0eab08ade87f684494af9669
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2016-07-19 14:02:33 +0100

    Use new hash literals syntax for rendering 404
    * Update the not_found function to use the Ruby 1.9 hash literals syntax (from lint)

--- app/controllers/components_controller.rb
+++ app/controllers/components_controller.rb
@@ -15,6 +15,6 @@ class ComponentsController < ApplicationController
   end
 
   def not_found
-    render :file => "#{Rails.root}/public/404", :layout => false, :status => :not_found
+    render file: "#{Rails.root}/public/404", layout: false, status: :not_found
   end
 end
