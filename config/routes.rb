9c456bccf810e97921aa89b7bd33ab4386cf8f15
Paul Hayes <fofr@users.noreply.github.com>
2017-08-16 18:16:47 +0100

    Remove about page and update index
    Link to the two component guides that exist

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount GovukPublishingComponents::Engine, at: "/static"
 
-  get 'about' => 'about#index'
+  get 'about' => to: redirect("/")
   get 'component', to: redirect("/static")
   get 'component/:id', to: redirect("/static/%{id}")
   root 'welcome#index'
